1fb0605f8c3a99f2e001ae46cf95008e1bcd5ef9
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2023-05-10 22:10:26 +0530

    done ig

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -1,28 +1,38 @@
-"use client"
 //@ts-nocheck
+"use client";
 import { FC } from "react";
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 interface TextProps {}
 
-
-
-
 const Text: FC<TextProps> = ({}) => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
 
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser does not support speech recognition.</span>;
+  }
 
   return (
     <div>
       <h1 className="text-5xl font-bold underline decoration-wavy">
         Speech to text
+        <p>Microphone: {listening ? 'on' : 'off'}</p>
       </h1>
       <p className=" mt-6 pb-32 mb-4 rounded-md bg-base-100">
         <span className="ml-2 font-bold text-xl">generated text:</span>
+        {transcript}
       </p>
       <div className="flex gap-3">
-        <button className="btn btn-primary">Copy To Clipboard</button>
-        <button className="btn btn-primary">Start Recording</button>
-        <button className="btn btn-primary">Stop Recording</button>
+        <button onClick={SpeechRecognition.startListening}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
       </div>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
+import 'regenerator-runtime/runtime'
 
 const inter = Inter({ subsets: ['latin'] })
 
