f110fb6472d2c37ea0d5b475090989d0a291c8d3
Arunava Ghosh <102473837+its-ag@users.noreply.github.com>
2023-05-10 22:23:47 +0530

    styling

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -23,16 +23,16 @@ const Text: FC<TextProps> = ({}) => {
     <div>
       <h1 className="text-5xl font-bold underline decoration-wavy">
         Speech to text
-        <p>Microphone: {listening ? 'on' : 'off'}</p>
       </h1>
-      <p className=" mt-6 pb-32 mb-4 rounded-md bg-base-100">
+      <p className=" mt-6 pb-32 mb-4 rounded-md bg-base-100 w-96 h-48">
         <span className="ml-2 font-bold text-xl">generated text:</span>
         {transcript}
       </p>
+        <p className="mb-2 text-xl font-bold">Microphone: {listening ? 'Listing to your voice..' : 'off'}</p>
       <div className="flex gap-3">
-        <button onClick={SpeechRecognition.startListening}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
+        <button className="btn btn-primary btn-sm" onClick={SpeechRecognition.startListening}>Start</button>
+        <button className="btn btn-secondary btn-sm" onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button className="btn btn-accent btn-sm" onClick={resetTranscript}>Reset</button>
       </div>
     </div>
   );
